c428ea4c597ad76163a9bfd8ad9652ef7dd4eef5
vidming <1083534068@qq.com>
2019-06-20 10:38:46 +0800

    增加了TestContoller文件

--- weixin_parent/weixin_web/src/main/java/com/bj1901/web/controller/TestContoller.java
+++ weixin_parent/weixin_web/src/main/java/com/bj1901/web/controller/TestContoller.java
@@ -46,54 +46,23 @@ public class TestContoller {
     public Object createMenu() {
 
         String paramBody = "{\n" +
-                "    \"button\": [\n" +
-                "        {\n" +
-                "            \"name\": \"扫码\", \n" +
-                "            \"sub_button\": [\n" +
-                "                {\n" +
-                "                    \"type\": \"scancode_waitmsg\", \n" +
-                "                    \"name\": \"扫码带提示\", \n" +
-                "                    \"key\": \"rselfmenu_0_0\", \n" +
-                "                    \"sub_button\": [ ]\n" +
-                "                }, \n" +
-                "                {\n" +
-                "                    \"type\": \"scancode_push\", \n" +
-                "                    \"name\": \"扫码推事件\", \n" +
-                "                    \"key\": \"rselfmenu_0_1\", \n" +
-                "                    \"sub_button\": [ ]\n" +
-                "                }\n" +
-                "            ]\n" +
-                "        }, \n" +
-                "        {\n" +
-                "            \"name\": \"发图\", \n" +
-                "            \"sub_button\": [\n" +
-                "                {\n" +
-                "                    \"type\": \"pic_sysphoto\", \n" +
-                "                    \"name\": \"系统拍照发图\", \n" +
-                "                    \"key\": \"rselfmenu_1_0\", \n" +
-                "                   \"sub_button\": [ ]\n" +
-                "                 }, \n" +
-                "                {\n" +
-                "                    \"type\": \"pic_photo_or_album\", \n" +
-                "                    \"name\": \"拍照或者相册发图\", \n" +
-                "                    \"key\": \"rselfmenu_1_1\", \n" +
-                "                    \"sub_button\": [ ]\n" +
-                "                }, \n" +
-                "                {\n" +
-                "                    \"type\": \"pic_weixin\", \n" +
-                "                    \"name\": \"微信相册发图\", \n" +
-                "                    \"key\": \"rselfmenu_1_2\", \n" +
-                "                    \"sub_button\": [ ]\n" +
-                "                }\n" +
-                "            ]\n" +
-                "        }, \n" +
-                "        {\n" +
-                "            \"name\": \"发送位置\", \n" +
-                "            \"type\": \"location_select\", \n" +
-                "            \"key\": \"rselfmenu_2_0\"\n" +
-                "        }\n" +
-                "    ]\n" +
-                "}";
+                "     \"button\":[\n" +
+                "     {    \n" +
+                "          \"type\":\"click\",\n" +
+                "          \"name\":\"石头\",\n" +
+                "          \"key\":\"shitou\"\n" +
+                "      },\n" +
+                "\t  {    \n" +
+                "          \"type\":\"click\",\n" +
+                "          \"name\":\"剪刀\",\n" +
+                "          \"key\":\"jiandao\"\n" +
+                "      },\n" +
+                "\t  {    \n" +
+                "          \"type\":\"click\",\n" +
+                "          \"name\":\"布\",\n" +
+                "          \"key\":\"bu\"\n" +
+                "      }\n" +
+                " }";
         Boolean menuSuccessFlag = menuService.createMenu(paramBody);
 
         return menuSuccessFlag;
